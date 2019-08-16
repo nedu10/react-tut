a4ca04b596185816c1167b22cc43eb8c8b40dd22
Nedu <cifediorah3@gmail.com>
2019-08-16 22:36:08 +0100

    complete basics of react

--- src/Components/PersonFunction/PersonFunction.js
+++ src/Components/PersonFunction/PersonFunction.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import './Person.css'  //Note this styling is not scoped
 
-const person = () => {
+const person = (props) => {
+
+    // const testing = (e) => {
+    //     console.log(e)
+    // }
+
     return (
-        <div>
-            <p>I am a person function</p>
+        <div className="Person" >
+            <p onClick={props.click}>I am {props.name} and i am {props.age} years old</p>
+            <p>{props.children}</p>
+            <input type="text" onChange = {(e) => props.changed(e.target.value)} value={props.name} />
         </div>
     )
 }
